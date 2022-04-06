145db2af2fb937d0afce53a39f95152b8b81d6ee
Don B <db1997@outlook.fr>
2022-04-06 19:57:20 +0200

    add hotels to admin dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Hotel;
+use App\Entity\Manager;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -42,6 +44,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        //yield MenuItem::linkToCrud('Hotel', 'fas fa-list', Hotel::class);
+        yield MenuItem::linkToCrud('Hotels', 'fas fa-hotel', Hotel::class);
+        //yield MenuItem::linkToCrud('Managers', 'fas fa-people', Manager::class);
     }
 }
