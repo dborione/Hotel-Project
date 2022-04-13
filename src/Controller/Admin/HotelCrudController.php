2f834ccad431e5cdb3b8fb2f591d8205c1330dd7
Don B <db1997@outlook.fr>
2022-04-13 21:58:15 +0200

    make new crud

--- src/Controller/Admin/HotelCrudController.php
+++ src/Controller/Admin/HotelCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Hotel;
+use App\Entity\Manager;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use Doctrine\Persistence\ObjectManager;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class HotelCrudController extends AbstractCrudController
@@ -16,6 +18,10 @@ class HotelCrudController extends AbstractCrudController
         return Hotel::class;
     }
 
+    public function load(ObjectManager $manager)
+    {
+        $manager = new Manager();
+    }
     
     public function configureFields(string $pageName): iterable
     {
@@ -30,6 +36,16 @@ class HotelCrudController extends AbstractCrudController
             TextField::new('hotelSlug'),
             TextEditorField::new('hotelDescription'),
             AssociationField::new('manager')
+           
+            TextField::new('managerLastName'),
+            TextField::new('managerFirstName'),
+            TextField::new('Email'),
+            TextField::new('Password'),
+            //ChoiceField::new('Roles');
+            //TextField::new('Password'),
+            //->setRoles(["ROLE_MANAGER"]);
+            //->add('userRoles')
+            )
         ];
 
 
