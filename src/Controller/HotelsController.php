19c8705ff0dabdeac976e76176069d014b45f463
Don B <db1997@outlook.fr>
2022-04-15 18:35:05 +0200

    create hotels page

--- src/Controller/HotelsController.php
+++ src/Controller/HotelsController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\HotelRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HotelsController extends AbstractController
 {
     #[Route('/hotels', name: 'app_hotels')]
-    public function index(): Response
+    public function index(HotelRepository $hotelRepository)
     {
         return $this->render('hotels/index.html.twig', [
-            'controller_name' => 'HotelsController',
+            'hotel' => $hotelRepository->findAll()
         ]);
     }
 }
